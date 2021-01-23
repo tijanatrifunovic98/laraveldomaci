f21cd95cb61732b48b790a8af9ecbfd66662d241
Tijana <trifunovic.tijana98@gmail.com>
2021-01-23 22:55:30 +0100

    Rute za stranice i dizajn

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/','App\Http\Controllers\PagesController@index');
+Route::get('/about','App\Http\Controllers\PagesController@about');
