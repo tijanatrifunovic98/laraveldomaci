ec20e26996ca89b6d5c1ac49ab940fb362a1abaa
Tijana <trifunovic.tijana98@gmail.com>
2021-01-28 13:16:07 +0100

    Popunjavanje about

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.app')
 @section('content')
           <h1>{{$title}}</h1>
-          <p>JDJIFN</p>      
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex odio! Eum labore fuga exercitationem iusto quia nisi voluptatem, beatae magni assumenda, est enim, blanditiis aperiam pariatur nobis ea distinctio.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex odio! Eum labore fuga exercitationem iusto quia nisi voluptatem, beatae magni assumenda, est enim, blanditiis aperiam pariatur nobis ea distinctio.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus dignissimos asperiores nostrum. Itaque sunt ea quam suscipit, non rem ipsa, saepe assumenda, ut laborum quasi iusto repellat blanditiis voluptate cum.</p>      
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex odio! Eum labore fuga exercitationem iusto quia nisi voluptatem, beatae magni assumenda, est enim, blanditiis aperiam pariatur nobis ea distinctio.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus dignissimos asperiores nostrum. Itaque sunt ea quam suscipit, non rem ipsa, saepe assumenda, ut laborum quasi iusto repellat blanditiis voluptate cum.</p>      
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex odio! Eum labore fuga exercitationem iusto quia nisi voluptatem, beatae magni assumenda, est enim, blanditiis aperiam pariatur nobis ea distinctio.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus dignissimos asperiores nostrum. Itaque sunt ea quam suscipit, non rem ipsa, saepe assumenda, ut laborum quasi iusto repellat blanditiis voluptate cum.</p>      
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex odio! Eum labore fuga exercitationem iusto quia nisi voluptatem, beatae magni assumenda, est enim, blanditiis aperiam pariatur nobis ea distinctio.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus dignissimos asperiores nostrum. Itaque sunt ea quam suscipit, non rem ipsa, saepe assumenda, ut laborum quasi iusto repellat blanditiis voluptate cum.</p>      
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex odio! Eum labore fuga exercitationem iusto quia nisi voluptatem, beatae magni assumenda, est enim, blanditiis aperiam pariatur nobis ea distinctio.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus dignissimos asperiores nostrum. Itaque sunt ea quam suscipit, non rem ipsa, saepe assumenda, ut laborum quasi iusto repellat blanditiis voluptate cum.</p>      
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex odio! Eum labore fuga exercitationem iusto quia nisi voluptatem, beatae magni assumenda, est enim, blanditiis aperiam pariatur nobis ea distinctio.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus dignissimos asperiores nostrum. Itaque sunt ea quam suscipit, non rem ipsa, saepe assumenda, ut laborum quasi iusto repellat blanditiis voluptate cum.</p>      
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex odio! Eum labore fuga exercitationem iusto quia nisi voluptatem, beatae magni assumenda, est enim, blanditiis aperiam pariatur nobis ea distinctio.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus dignissimos asperiores nostrum. Itaque sunt ea quam suscipit, non rem ipsa, saepe assumenda, ut laborum quasi iusto repellat blanditiis voluptate cum.</p>      
+
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus dignissimos asperiores nostrum. Itaque sunt ea quam suscipit, non rem ipsa, saepe assumenda, ut laborum quasi iusto repellat blanditiis voluptate cum.</p>      
 @endsection
