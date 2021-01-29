40e0cd35e5ca5b59fa10e31f2471b12532d68984
Tijana <trifunovic.tijana98@gmail.com>
2021-01-29 17:14:09 +0100

    dizajn home page

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
     <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">
+        <a class="navbar-brand" href="{{ url('/home') }}">
             {{ config('app.name', 'Travel') }}
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -14,7 +14,7 @@
             </ul>
             <ul class="navbar-nav mr-auto">
               <li class="nav-item active">
-                <a class="nav-link" href="/">Home
+                <a class="nav-link" href="/home">Home
                   <span class="sr-only">(current)</span>
                 </a>
               </li>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,6 +2,6 @@
 @section('content')
 <div class="jumbotron text-center">
           <h1>{{$title}}</h1>
-          <p>Index Strana Cao</p>     
+               
           <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a><a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p> 
 @endsection
